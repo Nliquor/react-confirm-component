e067a1942c9118954e47abb54d1b85f6ad0762e2
zn <1317476403@qq.com>
2019-12-07 21:48:41 +0800

    update

--- src/demo1.js
+++ src/demo1.js
@@ -1,37 +1,21 @@
-import React,{useState,useEffect} from 'react';
-/*
-	受控 value onChange 
-	非受控 defaultValue
-*/
-const  InputNumber = (props) =>{
-	let [value,setinnerValue] = useState(
-		props.defaultValue?props.defaultValue:props.value
-	)
-	useEffect(()=>{
-		props.value && setinnerValue(props.value)
-	},[props])
-	return (
-		<div>
-			<input value={value} onChange={(e)=>{
-				if(props.defaultValue){
-					setinnerValue(e.target.value)
-				}
-				props.onChange(e)
-			}}/>
-		</div>
-	)
-
+import React,{Component} from 'react';
+import confirm from './confirm'
+class App extends Component{
+	render() {
+		return(
+			<div>
+				000
+			</div>
+		)
+	}
+	async componentDidMount(){
+		let res = await confirm("确定删除吗")
+		if(res) {
+			console.log("是")
+		} else {
+			console.log("否")
+		}
+	}
 }
 
-function App(){
-    const [value,setValue] = useState('aaa')
-    return (
-        <div>
-        <InputNumber value={value} onChange={e=>{
-			// setValue(e.target.value)
-		}}/>
-        <InputNumber defaultValue={value} onChange={e=>{}}/>
-        </div>
-    )
-}
 export default App;
